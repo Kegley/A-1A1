6e41fa018159e10bebf57d7627b1189d23a03f45
Joshua Kegley <Josh1289op@gmail.com>
2016-05-13 17:11:20 -0400

    Added some functionality to help test server side

--- Pests-Client/script.js
+++ Pests-Client/script.js
@@ -1,4 +1,62 @@
 var exampleSocket = new WebSocket("ws://localhost:443");
-setTimeout(function() {
+
+exampleSocket.onmessage = function(event) {
+    console.log(event);
+}
+
+function antHill() {
+    console.log("Attacking Anthill");
+    sendMsg(encodeMsg(2, "Attacking Hill"));
+}
+
+function connect() {
+    if(exampleSocket.readyState == 1) {
+        console.log("Already Connected");
+        return;
+    }
+    exampleSocket = new WebSocket("ws://localhost:443");
+}
+
+function disconnect() {
     exampleSocket.close();
-}, 20000);
+}
+
+function eatFood() {
+    console.log("Ate Food");
+    sendMsg(encodeMsg(1, "Eating Food"));
+}
+
+function encodeMsg(id, msg) {
+    var encodedMsg = [];
+    encodedMsg.push(id);
+    encodedMsg.push(msg);
+    return encodedMsg;
+}
+
+function joinGame() {
+    console.log("Joined Game");
+    sendMsg(encodeMsg(0, "Joshy"));
+}
+
+function sendMsg(msg) {
+    exampleSocket.send(msg);
+}
+
+
+
+exampleSocket.onmessage = function(event) {
+  var f = document.getElementById("chatbox").contentDocument;
+  var text = "";
+  var msg = JSON.parse(event.data);
+  var time = new Date(msg.date);
+  var timeStr = time.toLocaleTimeString();
+
+  console.log(msg);
+};
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,10,0,2*Math.PI);
+ctx.stroke();
---
+++ README.md
@@ -0,0 +1,22 @@
+In Pests -
+
+Players play as Ants.
+    Hill - Color/Name:
+    Ant - Color
+    Three Functions:
+        Eat Food
+            - Player sends Ants to collect pieces of food
+            - Ants return Food to Hill
+            - Food only last as many units as there are.
+        Attack Neighboring Hills
+            - Player sends Ants to nearby Ant Hill
+            - Amount of food = Amount of Health
+            - Ants steal food and bring back to their Hill
+            - If Ant hill runs out of food - Hill Dies
+            - GAMEOVER
+        Consume Food
+            - Breed more Ants
+            - Upgrade Ants
+                - Speed
+                - Held Weight
+    
